d1f76dec43f2e3d8017427916a0032bb3e7984f8
vartika1403 <vartika.1403@gmail.com>
2017-06-05 22:56:14 +0530

    added google map

--- app/src/main/java/com/example/vartikasharma/backgroundlocationtracking/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/backgroundlocationtracking/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.vartikasharma.backgroundlocationtracking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
+    // Google Map
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            // Loading map
+            initilizeMap();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+
 }
