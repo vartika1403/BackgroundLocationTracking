bc09a45908556ce28e9ba7bc38463d4a05d52a31
vartika1403 <vartika.1403@gmail.com>
2017-06-08 14:22:54 +0530

    added code for checking internet connectiona ang gps

--- app/src/main/java/com/example/vartikasharma/backgroundlocationtracking/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/backgroundlocationtracking/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.vartikasharma.backgroundlocationtracking;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -14,10 +18,21 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     // Google Map
     private GoogleMap googleMap;
+
     // Declaring a Location Manager
     protected LocationManager locationManager;
+
+    //instance of LocationManager
+    protected LocationTracker locationTracker;
+
+    //Connection detector class
+    protected ConnectionDetector connectionDetector;
+
+    //flag for internet connection status
+    boolean isInternetPresent = false;
 
     @BindView(R.id.start_shift)
     public Button startShift;
@@ -30,6 +45,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        connectionDetector = new ConnectionDetector(this);
+
+        // check is internet present
+        isInternetPresent = connectionDetector.isConnectingToInternet();
+        if (!isInternetPresent) {
+            // Internet Connection is not present
+            showAlertDialog(MainActivity.this, "Internet Connection Error",
+                    "Please connect to working Internet connection", false);
+            // stop executing code by return
+            return;
+        }
+
         try {
             // Loading map
             initilizeMap();
@@ -37,6 +64,14 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        locationTracker = new LocationTracker(this);
+
+        //check if GPS Location can get
+        if (locationTracker.canGetLocation()) {
+            Log.d(LOG_TAG, "latitute: " + locationTracker.getLatitude() + ",longitute: " + locationTracker.getLongitude());
+        }
+
 
     }
 
@@ -47,11 +82,11 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * function to load map. If map is not created it will create it for you
-     * */
+     */
     private void initilizeMap() {
         if (googleMap == null) {
             googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-                    R .id.map)).getMap();
+                    R.id.map)).getMap();
 
             // check if map is created successfully or not
             if (googleMap == null) {
@@ -66,5 +101,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         initilizeMap();
+    }
+
+    public void showAlertDialog(Context context, String title, String message,
+                                Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+        // Setting Dialog Title
+        alertDialog.setTitle(title);
+
+        // Setting Dialog Message
+        alertDialog.setMessage(message);
+
+        if (status != null)
+            // Setting alert dialog icon
+            //  alertDialog.setIcon((status) ? R.drawable.success : R.drawable.fail);
+
+            // Setting OK Button
+            alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+
+        // Showing Alert Message
+        alertDialog.show();
     }
 }
